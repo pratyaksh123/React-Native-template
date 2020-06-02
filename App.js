a90ae8414c51e302e8cdfb08551f4f58c52fad26
Pratyaksh Tyagi <tyagipratyaksh@gmail.com>
2020-06-03 00:35:36 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Navigator from './App/Navigator/HomeStack';
+
+// Wrap Here with a HOC if required.
+export default () => {
+  return <Navigator />;
+};
---
+++ App/Screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Edit HomeScreen.js To get Started !!</Text>
+    </View>
+  );
+};
+export default HomeScreen;
